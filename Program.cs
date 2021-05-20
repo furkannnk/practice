297b2c347cb8c027d4cda1e50073c40bb42b9891
Furkan <karadurmusfurkan@gmail.com>
2021-05-21 00:31:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            string metin = "0";
+            do{
+            Console.WriteLine("Selam ben papağan uygulama!");
+            Console.WriteLine("Yazı yaz ama 0 basarsan bu uygulama kapanır");  
+            metin = Console.ReadLine();
+            isimKaydet(metin);
+            }while(metin != "0");
+
+            
+        }
+        static void isimKaydet(string metin)
+        {
+            string yazi = metin;
+            Console.WriteLine(yazi);
+        }
+
+    }
+}
